972dc96d0cda5aae2ed39256855644c155ff1e4e
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-04-07 15:09:18 +0300

    debounce function added

---
+++ src/scripts/utils.js
@@ -0,0 +1,7 @@
+const debounce = (func, delay = 1000) => {
+  let timeout;
+  return (...args) => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
