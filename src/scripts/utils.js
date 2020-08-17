f850fb3fdbde5043a6c1d1636babe5046c9bed71
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-08-17 14:36:40 +0300

    project restructuration

--- src/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-const debounce = (func, delay = 1000) => {
-  let timeout;
-  return (...args) => {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), delay);
-  };
-};
-
-export default debounce;
